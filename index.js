2d7f1105bd4050a01a11035e6769425cb62b2268
Orianalopardo <orilopardo123@gmail.com>
2023-06-14 19:30:23 -0300

    Primer commit

--- index.js
+++ index.js
@@ -1,37 +1,21 @@
+function verificarEdad() {
     let edad = 0;
 
     while (edad < 18) {
     edad = parseInt(prompt("Ingrese su edad:"));
-
-    if (isNaN(edad)) {
-    console.log("Por favor, ingrese un número válido.");
+    
+    if(isNaN(edad)) {
+    console.log("Por favor, ingrese un numero valido");
+    edad = 0;
     continue;
-}
-
+    
+    }
     if (edad < 18) {
-    console.log("Debe ser mayor de edad para ingresar.");
-}
-}
-console.log("Bienvenido/a. Puede ingresar.");
-
-/*crear un algoritmo utilizando un ciclo*/
-
-    let repetir = parseInt(prompt('Ingrese la cantidad de veces que se repite hola: '))
-    for (let index = 1; index <= repetir; index++) {
-        console.log('Hola')    
+        console.log("Debe ser mayor de edad para ingresar.");
+    }
     }
 
-    /*armar un simulador interactivo*/
-    let cartTotal = 0;
+    console.log("Bienvenido/a. Puede ingresar.");
+}
+    verificarEdad();
 
-    function addToCart(price) {
-      cartTotal += price;
-    
-      const cartItemsElement = document.getElementById("cart-items");
-      const cartItem = document.createElement("li");
-      cartItem.innerText = "Producto $" + price;
-      cartItemsElement.appendChild(cartItem);
-    
-      const cartTotalElement = document.getElementById("cart-total");
-      cartTotalElement.innerText = "Total: $" + cartTotal;
-    }
